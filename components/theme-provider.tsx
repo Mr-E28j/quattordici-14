945e130954d982ef713f1cc932a59c0ae1ce5bca
Mr-E28j <juliorsolanop@gmail.com>
2025-03-27 11:35:14 -0400

    Add missing files and fix imports

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,10 @@
 "use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps & { children: React.ReactNode }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
